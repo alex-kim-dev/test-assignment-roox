3748085e8e375e19842bc126df1b951d94dc1f30
Alex Kim <45559664+alex-kim-dev@users.noreply.github.com>
2022-06-27 17:20:06 +0500

    feat: add Sorting component

---
+++ src/features/users/components/Sorting/Sorting.tsx
@@ -0,0 +1,36 @@
+import { Button } from '~/components';
+import { SortingOptions, useSorting } from '~/stores/sorting';
+
+import css from './Sorting.module.scss';
+
+export const Sorting: React.FC = () => {
+  const [currentOption, setOption] = useSorting();
+
+  const options = (Object.keys(SortingOptions) as SortingOptions[]).filter(
+    (key) => key !== SortingOptions.default
+  );
+
+  const handleRadioClick = (option: SortingOptions) => () => {
+    setOption((prevOption) =>
+      option === prevOption ? SortingOptions.default : option
+    );
+  };
+
+  return (
+    <fieldset className={css.wrapper}>
+      <legend>Sorting</legend>
+      {options.map((option) => (
+        <Button key={option} active={currentOption === option} as='label'>
+          by {option}
+          <input
+            checked={currentOption === option}
+            className={css.radio}
+            name={option}
+            type='radio'
+            onClick={handleRadioClick(option)}
+          />
+        </Button>
+      ))}
+    </fieldset>
+  );
+};
