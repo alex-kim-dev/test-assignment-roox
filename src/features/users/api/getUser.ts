eb937dac79e33a387595edb92c7648a6a346abe8
Alex Kim <45559664+alex-kim-dev@users.noreply.github.com>
2022-06-28 15:44:54 +0500

    feat: add user fetching

---
+++ src/features/users/api/getUser.ts
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react';
+
+import type { Id, User } from '../types';
+
+export const getUser = async (id: Id): Promise<User> => {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/user/${id}`
+  );
+  const { ok, status, statusText } = response;
+  if (!ok) throw new Error(`${status} ${statusText}`);
+  return response.json();
+};
+
+export const useUser = (id: Id) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [isLoading, setLoading] = useState(false);
+  const [error, setError] = useState<Error | null>(null);
+
+  useEffect(() => {
+    setLoading(true);
+    getUser(id)
+      .then((data) => {
+        setUser(data);
+        setError(null);
+      })
+      .catch((err) => {
+        setUser(null);
+        setError(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [id]);
+
+  return { user, isLoading, error };
+};
